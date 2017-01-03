04d4203881b55351bb78c0d67ee7daadc1619b81
mattdixon85 <matt_dixon85@yahoo.co.uk>
2017-01-03 22:53:03 +0000

    updated hive regex escape

--- hive.sql
+++ hive.sql
@@ -6,5 +6,5 @@ from (
   ,${hiveconf:TS} as ts_expr
   ,${hiveconf:EXPR} as event_expr
 ) as sub
-select id_expr, regexpr_replace(concat_ws(' ',sort_array(collect_set(concat_ws('#',ts_expr, event_expr)))), '\d+#','') as seq
+select id_expr, regexpr_replace(concat_ws(' ',sort_array(collect_set(concat_ws('#',ts_expr, event_expr)))), '\\d+#','') as seq
 group by id_expr
